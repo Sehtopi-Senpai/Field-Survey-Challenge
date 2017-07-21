293f5c5fd78b00e28c78358a227813860ea2fce3
Josh Green <jg229@mail.missouri.edu>
2017-07-21 18:36:20 -0500

    added first enum

---
+++ Field Survey/Field Survey/Model/Classification.swift
@@ -0,0 +1,19 @@
+//
+//  Classification.swift
+//  Field Survey
+//
+//  Created by Lappy on 7/21/17.
+//  Copyright © 2017 xanda code. All rights reserved.
+//
+
+import Foundation
+
+enum Classification: String {
+    case reptile
+    case amphibian
+    case bird
+    case insect
+    case plant
+    case mammal
+    case fish
+}
